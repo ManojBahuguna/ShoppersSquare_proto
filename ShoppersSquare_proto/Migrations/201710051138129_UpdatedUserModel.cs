42c6d0d16298bf05325cd523861f9f121843a7d6
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2017-10-13 14:11:55 +0530

    removed table columns from user table

--- ShoppersSquare_proto/Migrations/201710051138129_UpdatedUserModel.cs
+++ ShoppersSquare_proto/Migrations/201710051138129_UpdatedUserModel.cs
@@ -7,16 +7,10 @@ namespace ShoppersSquare_proto.Migrations
     {
         public override void Up()
         {
-            AddColumn("dbo.AspNetUsers", "FirstName", c => c.String());
-            AddColumn("dbo.AspNetUsers", "LastName", c => c.String());
-            AddColumn("dbo.AspNetUsers", "Address", c => c.String());
         }
         
         public override void Down()
         {
-            DropColumn("dbo.AspNetUsers", "Address");
-            DropColumn("dbo.AspNetUsers", "LastName");
-            DropColumn("dbo.AspNetUsers", "FirstName");
         }
     }
 }
