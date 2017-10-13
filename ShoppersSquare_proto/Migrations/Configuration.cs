a40a334f10f63b171af97ed55a3749bd48e9fb32
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2017-10-13 15:29:22 +0530

    Add Categories

--- ShoppersSquare_proto/Migrations/Configuration.cs
+++ ShoppersSquare_proto/Migrations/Configuration.cs
@@ -17,6 +17,13 @@ namespace ShoppersSquare_proto.Migrations
 
         protected override void Seed(ShoppersSquare_proto.Models.ApplicationDbContext context)
         {
+            context.Categories.SqlQuery(@"
+                INSERT INTO [dbo].[ProductTypes] ([Id], [Name], [ImageUrl]) VALUES (0, N'Electronics', N'https://afatrading.co.ke/images/home.jpg')
+                INSERT INTO [dbo].[ProductTypes] ([Id], [Name], [ImageUrl]) VALUES (1, N'Home and Furniture', N'http://rizshops.com/images/furn1.jpg')
+                INSERT INTO [dbo].[ProductTypes] ([Id], [Name], [ImageUrl]) VALUES (2, N'Clothing and Accesories', N'http://www.summerhillbaths.co.uk/includes/templates/summerhillbaths//images/banner.jpg')
+                INSERT INTO [dbo].[ProductTypes] ([Id], [Name], [ImageUrl]) VALUES (3, N'Footwear', N'http://www.florsheim.com/shop/resources/images/index/index1-125-banner.jpg')
+                INSERT INTO [dbo].[ProductTypes] ([Id], [Name], [ImageUrl]) VALUES (4, N'Beauty and Personal Care', N'https://www.latestinbeauty.com/pub/static/frontend/lib/iammee/en_GB/Lib_IammeeCatalog/images/all-banner.jpg')
+            ");
             //var storeManagerRole = "StoreManager";
             //var userEmail = "storemanager@shoppers.square";
             //var userPassword = "monoTHEmanager";
